309b510ec10a929ef479ca19f765313fe82432d9
kushagar405 <soodachala16@gmail.com>
2025-02-13 11:55:07 +0530

    Solution to Logical OR Operation

--- Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
+++ Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
@@ -8,7 +8,7 @@ int main() {
     scanf("%d %d", &num1, &num2);
 
     // Check if at least one number is greater than zero
-    if (num1 > 0 || num2 > 0) {
+    if (num1 > 0 || num2 < 0) {
         printf("True");
     } else {
         printf("False");
